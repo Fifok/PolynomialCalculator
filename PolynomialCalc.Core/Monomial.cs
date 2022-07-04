771697e305f8e022e0b3aab5c6dd6726f59677b2
Fifok <fifok77@gmail.com>
2022-07-04 22:09:40 +0200

    Add Polynomial and Monomial classes

---
+++ PolynomialCalc.Core/Monomial.cs
@@ -0,0 +1,22 @@
+namespace PolynomialCalc.Core;
+
+public class Monomial
+{
+    public int Coefficient { get; set; }
+    public char Variable { get; set; }
+    public int Exponent { get; set; }
+
+    public Monomial(int coefficient, int exponent)
+    {
+        Coefficient = coefficient;
+        Variable = 'x';
+        Exponent = exponent;
+    }
+
+    public Monomial(int coefficient, char variable, int exponent)
+    {
+        Coefficient = coefficient;
+        Variable = variable;
+        Exponent = exponent;
+    }
+}
---
+++ PolynomialCalc.Core/Polynomial.cs
@@ -0,0 +1,19 @@
+﻿namespace PolynomialCalc.Core;
+public class Polynomial
+{
+    public List<Monomial> Monomials = new List<Monomial>();
+
+    public void Add(Monomial monomial)
+    {
+        var existingMonomial = Monomials
+            .SingleOrDefault(m => m.Variable == monomial.Variable && m.Exponent == monomial.Exponent);
+        
+        if(existingMonomial == null)
+        {
+            Monomials.Add(monomial);
+            return;
+        }
+
+        existingMonomial.Coefficient += monomial.Coefficient;
+    }
+}
