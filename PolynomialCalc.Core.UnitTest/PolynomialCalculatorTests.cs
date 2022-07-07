43d2253e66e7eba1b32cec313ec6df643a7b229b
Fifok <fifok77@gmail.com>
2022-07-07 22:09:30 +0200

    Refactor PolynomialCalculatorTests Add test.

--- PolynomialCalc.Core.UnitTest/PolynomialCalculatorTests.cs
+++ PolynomialCalc.Core.UnitTest/PolynomialCalculatorTests.cs
@@ -10,30 +10,30 @@ public class PolynomialCalculatorTests
     {
         //(5x^5+5x^3+6x^2-2x+5) + (6x^4-3x^3+7x^2-6x+12) = 6x^4+2x^3+13x^2-8x+17
         
-        var left = new Polynomial();
-        left.Add(new Monomial(-5,5));
-        left.Add(new Monomial(5,3));
-        left.Add(new Monomial(6,2));
-        left.Add(new Monomial(-2,1));
-        left.Add(new Monomial(5,0));
+        var left = new Polynomial()
+            .Add(new Monomial(-5,5))
+            .Add(new Monomial(5,3))
+            .Add(new Monomial(6,2))
+            .Add(new Monomial(-2,1))
+            .Add(new Monomial(5,0));
 
-        var right = new Polynomial();
-        right.Add(new Monomial(-5,5));
-        right.Add(new Monomial(6,4));
-        right.Add(new Monomial(-3,3));
-        right.Add(new Monomial(7,2));
-        right.Add(new Monomial(2,1));
-        right.Add(new Monomial(12,0));
+        var right = new Polynomial()
+            .Add(new Monomial(-5,5))
+            .Add(new Monomial(6,4))
+            .Add(new Monomial(-3,3))
+            .Add(new Monomial(7,2))
+            .Add(new Monomial(2,1))
+            .Add(new Monomial(12,0));
 
         var calculator = new PolynomialCalculator();
         var result = calculator.Add(left, right);
         
-        var expectedPolynomial = new Polynomial();
-        expectedPolynomial.Add(new Monomial(-10,5));
-        expectedPolynomial.Add(new Monomial(6,4));
-        expectedPolynomial.Add(new Monomial(2,3));
-        expectedPolynomial.Add(new Monomial(13,2));
-        expectedPolynomial.Add(new Monomial(17,0));
+        var expectedPolynomial = new Polynomial()
+            .Add(new Monomial(-10,5))
+            .Add(new Monomial(6,4))
+            .Add(new Monomial(2,3))
+            .Add(new Monomial(13,2))
+            .Add(new Monomial(17,0));
 
         result.Monomials.Should().BeEquivalentTo(expectedPolynomial.Monomials);
     }
