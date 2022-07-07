8764bd6161cc5b98b439ba7cb07151344da7ba7c
Fifok <fifok77@gmail.com>
2022-07-07 22:04:34 +0200

    Revert "Move polynomial add test to PolynomialTests."
    This reverts commit 50513cc8a6fcd84bfbcf0926ee4b23118ce8d979.

--- PolynomialCalc.Core.UnitTest/PolynomialTests.cs
+++ PolynomialCalc.Core.UnitTest/PolynomialTests.cs
@@ -75,37 +75,4 @@ public class PolynomialTests
         monomial.Variable.Should().Be(null);
         monomial.Coefficient.Should().Be(11);
     }
-
-     [Test]
-    public void Add_Should_AddTwoPolynomials_Correctly()
-    {
-        //(5x^5+5x^3+6x^2-2x+5) + (6x^4-3x^3+7x^2-6x+12) = 6x^4+2x^3+13x^2-8x+17
-        
-        var left = new Polynomial();
-        left.Add(new Monomial(-5,5))
-            .Add(new Monomial(5,3))
-            .Add(new Monomial(6,2))
-            .Add(new Monomial(-2,1))
-            .Add(new Monomial(5,0));
-
-        var right = new Polynomial();
-        right.Add(new Monomial(-5,5))
-             .Add(new Monomial(6,4))
-             .Add(new Monomial(-3,3))
-             .Add(new Monomial(7,2))
-             .Add(new Monomial(2,1))
-             .Add(new Monomial(12,0));
-
-        left.Add(right);
-
-        var expectedPolynomial = new Polynomial();
-        expectedPolynomial
-            .Add(new Monomial(-10,5))
-            .Add(new Monomial(6,4))
-            .Add(new Monomial(2,3))
-            .Add(new Monomial(13,2))
-            .Add(new Monomial(17,0));
-
-        left.Monomials.Should().BeEquivalentTo(expectedPolynomial.Monomials);
-    }
 }
