bf43dfd86519c3fafaea361c2c2759fb778e5f26
Fifok <fifok77@gmail.com>
2022-07-11 22:53:43 +0200

    Add PolynomialCalc.Application project with PolynomialService.

---
+++ PolynomialCalc.Application/PolynomialService.cs
@@ -0,0 +1,23 @@
+﻿using PolynomialCalc.Core;
+
+namespace PolynomialCalc.Applicatiob;
+
+public class PolynomialService
+{
+    private IPolynomialParser _parser;
+    private IPolynomialCalculator _calculator;
+
+    public PolynomialService(IPolynomialParser parser, IPolynomialCalculator calculator)
+    {
+        _parser = parser;
+        _calculator = calculator;
+    }
+
+    public Polynomial Add(string leftPolynomialText, string rightPolynomialText)
+    {
+        var leftPolynomial = _parser.Parse(leftPolynomialText);
+        var rightPolynomial = _parser.Parse(rightPolynomialText);
+
+        return _calculator.Add(leftPolynomial, rightPolynomial);
+    }
+}
