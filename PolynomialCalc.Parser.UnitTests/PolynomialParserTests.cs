204a492b7aae41d1d7cd964632b679f3353c1514
Kamil Krańczuk <31856712+Fifok@users.noreply.github.com>
2022-07-20 13:52:58 +0200

    Add PolynomialCalc.Parser.UnitTests project.

---
+++ PolynomialCalc.Parser.UnitTests/PolynomialParserTests.cs
@@ -0,0 +1,51 @@
+using FluentAssertions;
+using PolynomialCalc.Core;
+using PolynomialCalc.Parsers;
+
+namespace PolynomialCalc.Parser.UnitTests;
+
+public class PolynomialParserTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Parse_ShouldParse_PolynomialWithXVariable()
+    {
+        var parser = new PolynomialParser();
+        var textPolynomial = "5x^5 + 4x^3 - 5x^2 + 2x - 4"; 
+        
+        var result = parser.Parse(textPolynomial);
+
+        var expectedPolynomial = new Polynomial()
+            .Add(new Monomial(5, 5))
+            .Add(new Monomial(4, 3))
+            .Add(new Monomial(-5, 2))
+            .Add(new Monomial(2, 1))
+            .Add(new Monomial(-4, 0));
+
+        result.Should().BeEquivalentTo(expectedPolynomial);
+    }
+    
+    [Test]
+    public void Parse_ShouldParse_PolynomialWithDifferentVariables()
+    {
+        var parser = new PolynomialParser();
+        var textPolynomial = "5x^5 + 4y^3 - 5z^2 + 2x - 4y^2 + 5z + 4"; 
+        
+        var result = parser.Parse(textPolynomial);
+
+        var expectedPolynomial = new Polynomial()
+            .Add(new Monomial(5, 5))
+            .Add(new Monomial(4, 'y',3))
+            .Add(new Monomial(-5, 'z',2))
+            .Add(new Monomial(2, 1))
+            .Add(new Monomial(-4, 'y',2))
+            .Add(new Monomial(5, 'z',1))
+            .Add(new Monomial(4,0));
+
+        result.Should().BeEquivalentTo(expectedPolynomial);
+    }
+}
