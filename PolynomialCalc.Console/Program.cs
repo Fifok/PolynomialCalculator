664cfa3374096ca5c044f40d2b1caebc6ac85701
Kamil Krańczuk <31856712+Fifok@users.noreply.github.com>
2022-07-20 14:28:11 +0200

    Modify console Program.cs

--- PolynomialCalc.Console/Program.cs
+++ PolynomialCalc.Console/Program.cs
@@ -3,18 +3,22 @@
 using PolynomialCalc.Applicatiob;
 using PolynomialCalc.Core;
 using PolynomialCalc.Parsers;
+using PolynomialCalc.Presenters;
 
 Console.WriteLine("Welcome to Polynomial Calculator!");
 
-Console.WriteLine("Enter first polynomial (eg. 2x^3 + 5x^2 - 3x - 5):\n");
+Console.Write("Enter first polynomial (eg. 2x^3 + 5x^2 + 7y^2 - 3x - 5): ");
 var firstPolynomialText = Console.ReadLine();
-Console.WriteLine("Enter second polynomial (eg. 2x^3 + 5x^2 - 3x - 5):\n");
+Console.Write("Enter second polynomial (eg. 4x^3 + 7x^2 - 5y^2 + 6x - 52): ");
 var secondPolynomialText = Console.ReadLine();
 
 var result = new PolynomialService(new PolynomialParser(), new PolynomialCalculator())
         .Add(firstPolynomialText, secondPolynomialText);
+        
+var presenter = new PolynomialPresenter();
 
-foreach (var mono in result.Monomials)
-{
-        Console.WriteLine($"{mono.Coefficient}{mono.Variable}^{mono.Exponent}");
-}
+Console.WriteLine("Start calculation.");
+Console.WriteLine();
+
+Console.Write($"Adding: ({firstPolynomialText}) + ({secondPolynomialText}) = ");
+Console.WriteLine(presenter.RepresentAsString(result));
