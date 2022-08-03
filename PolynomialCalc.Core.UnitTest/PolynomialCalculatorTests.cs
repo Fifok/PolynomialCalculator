484a9d79d046f0b1bbfd7dc17387f2030d84e144
Kamil Krańczuk <31856712+Fifok@users.noreply.github.com>
2022-08-03 14:11:13 +0200

    Add test for subtracting polynomials.

--- PolynomialCalc.Core.UnitTest/PolynomialCalculatorTests.cs
+++ PolynomialCalc.Core.UnitTest/PolynomialCalculatorTests.cs
@@ -9,8 +9,6 @@ public class PolynomialCalculatorTests
     [Test]
     public void Add_Should_AddTwoPolynomials_Correctly()
     {
-        //(5x^5+5x^3+6x^2-2x+5) + (6x^4-3x^3+7x^2-6x+12) = 6x^4+2x^3+13x^2-8x+17
-
         var left = PrepareLeftPolynomial();
         var right = PrepareRightPolynomial();
 
@@ -18,17 +16,30 @@ public class PolynomialCalculatorTests
         calculator.SetOperation(new AddingOperation());
         var result = calculator.Execute(left, right);
         
-        CheckAddedPolynomialsConstancy(left, right);
-        CheckExpectedPolymonialAddResult(result);
+        CheckPolynomialsConstancy(left, right);
+        CheckExpectedPolynomialResult(result, PrepareExpectedPolynomialAddResult());
     }
-
-    private static void CheckExpectedPolymonialAddResult(Polynomial result)
+    
+    [Test]
+    public void Subtract_Should_SubtractTwoPolynomials_Correctly()
     {
-        var expectedPolynomial = PrepareExpectedPolynomialAddResult();
-        result.Monomials.Should().BeEquivalentTo(expectedPolynomial.Monomials);
+        var left = PrepareLeftPolynomial();
+        var right = PrepareRightPolynomial();
+
+        var calculator = new PolynomialCalculator();
+        calculator.SetOperation(new SubtractingOperation());
+        var result = calculator.Execute(left, right);
+        
+        CheckPolynomialsConstancy(left, right);
+        CheckExpectedPolynomialResult(result, PrepareExpectedPolynomialSubtractResult());
     }
 
-    private void CheckAddedPolynomialsConstancy(Polynomial left, Polynomial right)
+    private static void CheckExpectedPolynomialResult(Polynomial result, Polynomial expected)
+    {
+        result.Monomials.Should().BeEquivalentTo(expected.Monomials);
+    }
+
+    private void CheckPolynomialsConstancy(Polynomial left, Polynomial right)
     {
         left.Should().BeEquivalentTo(PrepareLeftPolynomial());
         right.Should().BeEquivalentTo(PrepareRightPolynomial());
@@ -43,7 +54,26 @@ public class PolynomialCalculatorTests
             .Add(new Monomial(13, 2))
             .Add(new Monomial(17, 0));
     }
-
+    
+    private static Polynomial PrepareExpectedPolynomialSubtractResult()
+    {
+        return new Polynomial()
+            .Add(new Monomial(-6, 4))
+            .Add(new Monomial(8, 3))
+            .Add(new Monomial(-1, 2))
+            .Add(new Monomial(-4, 1))
+            .Add(new Monomial(-7, 0));
+    }
+    
+    private Polynomial PrepareLeftPolynomial()
+    {
+        return new Polynomial()
+            .Add(new Monomial(-5,5))
+            .Add(new Monomial(5,3))
+            .Add(new Monomial(6,2))
+            .Add(new Monomial(-2,1))
+            .Add(new Monomial(5,0));
+    }
     private static Polynomial PrepareRightPolynomial()
     {
         return new Polynomial()
@@ -53,16 +83,6 @@ public class PolynomialCalculatorTests
             .Add(new Monomial(7, 2))
             .Add(new Monomial(2, 1))
             .Add(new Monomial(12, 0));
-    }
-
-    private Polynomial PrepareLeftPolynomial()
-    {
-        return new Polynomial()
-            .Add(new Monomial(-5,5))
-            .Add(new Monomial(5,3))
-            .Add(new Monomial(6,2))
-            .Add(new Monomial(-2,1))
-            .Add(new Monomial(5,0));
     }
 
 }
